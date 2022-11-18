532eecd42dcb628a5485816e6f08e2f37baaa883
kalebrobi <kalebrobi10@gmail.com>
2022-11-18 10:38:29 -0700

    l

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,9 +3,6 @@ const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
 
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
