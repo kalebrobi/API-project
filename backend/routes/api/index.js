94a375e674324774071693e2c58536c80c4813f9
kalebrobi <kalebrobi10@gmail.com>
2022-11-21 21:51:55 -0700

    api docs and api/index file update

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const { restoreUser } = require("../../utils/auth.js");
+const { requireAuth } = require('../../utils/auth')
+//const { restoreUser } = require("../../utils/auth.js");
 
-router.use(restoreUser);
+router.get('/test', requireAuth, (req, res) => {
+  res.json({ message:'success' })
+})
+
+
+//router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
@@ -13,24 +20,26 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user });
-});
 
-router.get(
-  '/restore-user',
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
 
 
