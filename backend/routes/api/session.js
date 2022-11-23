c94a84fb736c083ee0cf55146f2f2559a7853ace
kalebrobi <kalebrobi10@gmail.com>
2022-11-23 14:31:10 -0700

    amended the restoreUser function so it returns the proper user object

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -57,15 +57,26 @@ router.delete(
 );
 
 // Restore session user
+// router.get(
+//   '/',
+//   restoreUser,
+//   (req, res) => {
+//     const { user } = req;
+//     if (user) {
+//       return res.json({
+//         user: user.toSafeObject()
+//       });
+//     } else return res.json({});
+//   }
+// );
+
 router.get(
   '/',
   restoreUser,
   (req, res) => {
     const { user } = req;
     if (user) {
-      return res.json({
-        user: user.toSafeObject()
-      });
+      return res.json( user.toSafeObject() );
     } else return res.json({});
   }
 );
