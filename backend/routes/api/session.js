3c8af705226698d891fb45304fee3218ba9714e7
kalebrobi <kalebrobi10@gmail.com>
2022-11-23 14:18:31 -0700

    amended the res.json in the session login rout

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -34,10 +34,15 @@ router.post(
       return next(err);
     }
 
-    await setTokenCookie(res, user);
+    const token = await setTokenCookie(res, user);
 
     return res.json({
-      user
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username,
+      token
     });
   }
 );
